5464199a3d7038fc11d738c4e2be28f1ff18e2f0
Jameson.Brown <jameson.brown@mx.com>
2020-11-06 01:02:10 -0700

    Updated formatting and created option for elefant state

--- index.js
+++ index.js
@@ -7,103 +7,177 @@ let templates = require('./templates/templates.js')
 
 // Variables
 let appName = process.argv[2]
+let options = process.argv.slice(2)
+let configElefantState = options && options.includes('-es')
 let appDirectory = `${process.cwd()}/${appName}`
 
 // Main Run Function
 const run = async () => {
   // Start
-  console.log(`Creating ${appName}...\n`)
+  log(colors.bold(`\n\u262E New FE App\n`))
+  log(`Creating ${appName} app...`.cyan)
+  log(`This may take a moment.`.cyan)
   // Step 1
   let success = await createReactApp()
   if (!success) {
-    console.log(
-      'Something went wrong while trying to create a new React app using create-react-app'.red,
-    )
+    log('Something went wrong while trying to create a new React app using create-react-app'.red)
     return false
   }
+  gEnd()
+  vLine()
+
   // Step 2
   await cdIntoNewApp()
   // Step 3
   await installPackages()
+  vLine()
   // Step 4
   await updateTemplates()
+  vLine()
   // Finish
-  console.log(colors.bold.brightGreen(`Congrats you have created ${appName}\n`))
-  console.log(colors.bold.brightRed('VERY IMPORTANT:'))
-  console.log(
-    'Update the .env file(.env.example > .env) at the root of your project and add firebase and other variables.\n',
-  )
-  console.log(`cd ${appName}\n`)
-  console.log('yarn start\n')
+  log(colors.bold.brightGreen(`\nCongrats you have created ${appName}`))
+  // Instructions
+  await logInstructions()
 }
 
 // Sub Functions
 const createReactApp = () => {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     if (appName) {
-      shell.exec(`npx create-react-app ${appName}`, { silent: true }, code => {
+      shell.exec(`npx create-react-app ${appName}`, { silent: true }, (code) => {
         if (code == 1) {
           resolve(false)
         } else {
-          console.log('Created react app')
+          gStart()
+          log(colors.brightGreen('\u2714 Created new app\n'))
+          gEnd()
           resolve(true)
         }
       })
     } else {
-      console.log('\nNo app name was provided.'.red)
-      console.log('\nProvide an app name in the following format: ')
-      console.log('\nnew-fe-app ', 'app-name\n'.cyan)
+      log('\nNo app name was provided.'.red)
+      log('\nProvide an app name in the following format: ')
+      log('\nnew-fe-app ', 'app-name\n'.cyan)
       resolve(false)
     }
   })
 }
 const cdIntoNewApp = () => {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     shell.exec(`cd ${appName}`, () => {
       resolve()
     })
   })
 }
 const installPackages = () => {
-  return new Promise(resolve => {
-    console.log('\nInstalling styled-components, firebase, dotenv and react-router-dom'.cyan)
+  return new Promise((resolve) => {
+    log(`\nInstalling styled-components, firebase, dotenv and react-router-dom`.cyan)
+    configElefantState && log(`Installing optional elefant-state`.cyan)
     shell.exec(
-      `yarn add styled-components firebase dotenv react-router-dom`,
+      `npm i styled-components firebase dotenv react-router-dom ${
+        configElefantState && 'elefant-state'
+      }`,
       { silent: true },
       () => {
-        console.log(colors.brightGreen('\nFinished installing 4 packages\n'))
+        gStart()
+        log(colors.brightGreen('\u2714 Finished installing packages\n'))
+        gEnd()
         resolve()
       },
     )
   })
 }
 const updateTemplates = () => {
-  return new Promise(resolve => {
+  log(`\nUpdating files per template`.cyan)
+  return new Promise((resolve) => {
     let promises = []
+    // Override files with new-fe-app templates
     Object.keys(templates).forEach((fileName, i) => {
-      promises[i] = new Promise(res => {
-        fs.writeFile(`${appDirectory}/${fileName}`, templates[fileName], function(err) {
+      promises[i] = new Promise((res) => {
+        fs.writeFile(`${appDirectory}/${fileName}`, templates[fileName], function (err) {
           if (err) {
-            return console.log(err)
+            return log(err)
           }
           res()
         })
       })
     })
+    // If elefant state option is true, add import and provider to index.js
+    if (configElefantState) {
+      log(`Adding elefant-state`.cyan)
+      var text = `import React from 'react'
+      import ReactDOM from 'react-dom'
+      import './index.css'
+      import App from './App'
+      import { BrowserRouter as Router } from 'react-router-dom'
+      import { ElefantProvider } from 'elefant'
+      
+      require('dotenv').config()
+
+      export const initialState = {}
+      export const reducer = (state, action) => {
+        switch (action.type) {
+          case 'default':
+            return state
+          default:
+            return state
+        }
+      }
+      
+      ReactDOM.render(
+        <Router>
+          <ElefantProvider reducer={reducer} initialState={initialState}>
+            <App />
+          </ElefantProvider>
+        </Router>,
+        document.getElementById('root'),
+      )`
+
+      fs.writeFile(`${appDirectory}/src/index.js`, text, function (err) {
+        if (err) return log(err)
+      })
+    }
+    // Remove unused files
     try {
       fs.unlinkSync(`${appDirectory}/src/App.css`)
       fs.unlinkSync(`${appDirectory}/src/App.test.js`)
       fs.unlinkSync(`${appDirectory}/src/logo.svg`)
-      fs.unlinkSync(`${appDirectory}/src/serviceWorker.js`)
+      fs.unlinkSync(`${appDirectory}/src/reportWebVitals.js`)
       fs.unlinkSync(`${appDirectory}/src/setupTests.js`)
     } catch (err) {
       console.error(err)
     }
     Promise.all(promises).then(() => {
+      gStart()
+      log(colors.brightGreen('\u2714 Finished updating files\n'))
+      gEnd()
       resolve()
     })
   })
 }
+const logInstructions = () => {
+  return new Promise((resolve) => {
+    gStart()
+    gStart(colors.bold(`To begin developing:`))
+    log(`- Run \`cd ${appName}\``)
+    log('- Update the .env file(.env.example > .env) at the root of your project')
+    log('- Add firebase and other variables')
+    log('- Run `npm start`\n')
+    gEnd()
+    gEnd()
+    resolve()
+  })
+}
+
+// Helper Utils
+const vLine = () => {
+  const isSmall = process.stdout.columns < 75
+  log('_'.repeat(isSmall ? process.stdout.columns : 75))
+  log('-'.repeat(isSmall ? process.stdout.columns : 75))
+}
+const log = (content) => console.log(content)
+const gStart = (content) => console.group(content || '')
+const gEnd = () => console.groupEnd()
 
 // Run Script
 run()
